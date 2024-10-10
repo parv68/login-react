d86b8099da3e63129b751051dbaaee2f87ab7f40
Parv <parvruhil68@gmail.com>
2024-10-10 10:49:48 +0530

    updated jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
+import { useState } from 'react';
 import Form from './form';
-var userIsRegistered = true;
+
 export default function App() {
+  const [isRegistered, setIsRegistered] = useState(true); // Tracks if showing login or register form
+
   return (
     <>
-    <div className='container flex items-center justify-center'>
-      <Form IsRegistered={userIsRegistered} />
-    </div>
+      <div className='container flex items-center justify-center'>
+        <Form isRegistered={isRegistered} toggleForm={() => setIsRegistered(!isRegistered)} />
+      </div>
     </>
-  )
+  );
 }
+
--- src/form.jsx
+++ src/form.jsx
@@ -1,13 +1,20 @@
-function Form(props){
-    return(
-        <form action="" className="flex flex-col items-center justify-center my-10">
-            <input type="text" placeholder="Username" className="p-1 m-1" />
-            <input type="password" placeholder="password" className='p-1 m-1' />
-            {!props.isRegistered && (
-                <input type="password" placeholder="Confirm Password" className='p-1 m-1' />
-            )}
-            <button type="submit" className='text-white bg-blue-800 px-10 m-1'>{props.isRegistered ? 'Login':'Register'}</button>
-        </form>
-    )
-}
-export default Form
+function Form(props) {
+    return (
+      <form className="flex flex-col items-center justify-center my-10">
+        <input type="text" placeholder="Username" className="p-1 m-1" />
+        <input type="password" placeholder="Password" className='p-1 m-1' />
+        {!props.isRegistered && (
+          <input type="password" placeholder="Confirm Password" className='p-1 m-1' />
+        )}
+        <button type="submit" className='text-white bg-blue-800 px-10 m-1'>
+          {props.isRegistered ? 'Login' : 'Register'}
+        </button>
+        <a href="#" onClick={props.toggleForm}>
+          {props.isRegistered ? 'New user? Register here' : 'Already a user? Login here'}
+        </a>
+      </form>
+    );
+  }
+  
+  export default Form;
+  
